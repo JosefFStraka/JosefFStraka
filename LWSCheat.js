4ce681471e5709f59c227ee56d4251fc01eb904b
JosefFStraka <71689727+JosefFStraka@users.noreply.github.com>
2021-04-19 11:31:16 +0200

    Update LWSCheat.js

--- LWSCheat.js
+++ LWSCheat.js
@@ -1,3 +1,4 @@
+console.log("%c¯\\_(ツ)_/¯", "font-size:2em;color:red;background:black;border:1px solid yellow;padding:0.5em")
 let selectboxs = document.querySelectorAll(".selectbox")
 let editabledivs = document.querySelectorAll(".editablediv")
 let selectabledivs = document.querySelectorAll(".selectablediv")
@@ -10,19 +11,19 @@ for (let i = 0; i < selectabledivs.length; i++) {
 
 //editablediv
 for (let i = 0; i < editabledivs.length; i++) {
-    editabledivs[i].innerText = contenidoaguardar[i][0];
+    editabledivs[i].innerText = contenidoaguardar[i][0]
 }
 
 //selectbox
 for (var i = 0;i < selectboxs.length;i++) {
-  const box = selectboxs[i];
-  const options = box.options;
-  var rightOption = -1;
+  const box = selectboxs[i]
+  const options = box.options
+  var rightOption = -1
   for (var j = 0; j < options.length; j++) {
     if (options[j].value == true) {
-      rightOption = j;
-      options.selectedIndex = j;
-      break;
+      rightOption = j
+      options.selectedIndex = j
+      break
     }
   }
   /*if (rightOption == -1) {
@@ -32,16 +33,16 @@ for (var i = 0;i < selectboxs.length;i++) {
 
 //drag & drop
 for (let i = 0; i < contenidoaguardar.length; i++) {
-    let drag_number = "";
+    let drag_number = ""
     if (contenidoaguardar[i][0].includes("drag:")) {drag_number = contenidoaguardar[i][0].replace("drag:","")} else continue;
     for (let j = 0; j < contenidoaguardar.length; j++) {
-        let drop_number = "";
+        let drop_number = ""
         if (contenidoaguardar[j][0].includes("drop:")) {drop_number = contenidoaguardar[j][0].replace("drop:","")} else continue;
         if (drag_number == drop_number) {
-            let drag_el = document.getElementById("dragablediv"+i);
-            let drop_el = document.getElementById("dropdiv"+j);
-            drag_el.style.top = drop_el.style.top;
-            drag_el.style.left = drop_el.style.left;
+            let drag_el = document.getElementById("dragablediv"+i)
+            let drop_el = document.getElementById("dropdiv"+j)
+            drag_el.style.top = drop_el.style.top
+            drag_el.style.left = drop_el.style.left
         }
     }
 }
